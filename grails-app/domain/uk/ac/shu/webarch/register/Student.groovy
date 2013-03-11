bf3aa788cef4786e2ae9654cb3233f751415f7c5
root <root@WebArch.localdomain>
2013-03-11 13:21:31 +0000

    Fixed the controllers

--- grails-app/domain/uk/ac/shu/webarch/register/Student.groovy
+++ grails-app/domain/uk/ac/shu/webarch/register/Student.groovy
@@ -2,6 +2,30 @@ package uk.ac.shu.webarch.register
 
 class Student {
 
+
+
+String fullStudentName
+String studentNumber
+String notes
+
+Set courses
+
+Set classAtts
+
+static hadMany = [
+courses: Enrollment,
+classAtts: RegisterEntry
+]
+
+static mappedBy = [
+courses: "student",
+classAtts:"student"
+]
+
+
+
     static constraints = {
+fullStudentName(nullable:false, blank:false,maxSize:256);
+studentNumber(nullable:false, blank:false,maxSize:256);
     }
 }
